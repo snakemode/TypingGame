11ee84bd0c9129f9bf375674acae21b171655006
David Whitney <david@davidwhitney.co.uk>
2021-09-18 09:42:48 +0100

    Use getElementById to grab our elements using the DOM. Add empty startGame function.

---
+++ script.js
@@ -0,0 +1,9 @@
+const quote = document.getElementById('quote');
+const input = document.getElementById('typed-value');
+const start = document.getElementById('start');
+
+function startGame() {
+    console.log("Game started!");
+}
+
+start.addEventListener('click', startGame);
